373923e24691f0c0c370a5864a8de08ee2c1b551
Mike Doherty <mdoherty@reddit.com>
2018-07-18 11:41:57 -0700

    add targeting for SimpleExperiment

--- baseplate/experiments/__init__.py
+++ baseplate/experiments/__init__.py
@@ -171,8 +171,11 @@ class Experiments(object):
         :rtype: :py:class:`str`
         :return: Variant name if a variant is active, None otherwise.
         """
-
-        experiment = self._get_experiment(name)
+        try:
+            experiment = self._get_experiment(name)
+        except (ValueError, TypeError) as err:
+            logger.error("Invalid configiguration for experiment {}: {}".format(name, err))
+            return None
 
         if experiment is None:
             return None
@@ -233,7 +236,12 @@ class Experiments(object):
 
         """
 
-        experiment = self._get_experiment(experiment_name)
+        try:
+            experiment = self._get_experiment(experiment_name)
+        except:
+            logger.warning("Invalid configiguration for experiment {}"
+                .format(experiment_name))
+            return None
 
         if experiment is None:
             return
